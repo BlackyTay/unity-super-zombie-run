ed87db7ce036644531fa691ab9c78b7cfacb2df2
Tay Zheng Qiang <tay@outlook.my>
2020-02-18 17:17:15 +0800

    TiledBackground [ Scripts ]

--- Assets/Scripts/TiledBackground.cs
+++ Assets/Scripts/TiledBackground.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class TiledBackground : MonoBehaviour
 {
-    public int TextureSize = 32;
+    public int textureSize = 32;
     // Start is called before the first frame update
     void Start()
     {
-        
+        var newWidth = Mathf.Ceil(Screen.width / (textureSize * PixelPerfectCamera.scale));
+        var newHeight = Mathf.Ceil(Screen.height / (textureSize * PixelPerfectCamera.scale));
+
+        transform.localScale = new Vector3 (newWidth * textureSize, newHeight * textureSize, 1);
+
+        GetComponent<Renderer>().material.mainTextureScale = new Vector3 (newWidth, newHeight, 1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
